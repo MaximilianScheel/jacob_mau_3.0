e4817786d06a69c31a3a9adf976f98ed6aca3d28
Maximilian Scheel <scheel.development@gmail.com>
2023-12-23 01:37:25 +0100

    architectur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CarouselComponent }
+];
+
+
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CommonModule, SlickCarouselModule, NgbModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent {
+
+  slides = [
+    { img: "/assets/BER_AKTUELLES_1.jpeg", caption: "Beschreibung 1" },
+    { img: "/assets/BER_AKTUELLES_2.jpeg", caption: "Beschreibung 2" },
+  ];
+
+  slideConfig = {
+    "slidesToShow": 1,
+    "slidesToScroll": 1,
+    "dots": false,
+    "infinite": true,
+    "autoplay": true,
+    "autoplaySpeed": 3000,
+    "arrows": false,
+    "responsive": [
+      {
+        "breakpoint": 1024,
+        "settings": {
+          "slidesToShow": 1,
+          "slidesToScroll": 1,
+          "infinite": true,
+          "dots": true
+        }
+      },
+      {
+        "breakpoint": 600,
+        "settings": {
+          "slidesToShow": 1,
+          "slidesToScroll": 2
+        }
+      },
+      {
+        "breakpoint": 480,
+        "settings": {
+          "slidesToShow": 1,
+          "slidesToScroll": 1
+        }
+      }
+    ]
+  };
+}
